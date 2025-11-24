7f785d8ebd3c570c92c10d721fdf12a6e9ef6a71
tletle7102 <tletle7102@gmail.com>
2025-11-24 11:37:00 +0900

    백준1001 풀이: https://www.acmicpc.net/problem/1001

---
+++ src/baekjoon/Problem1001.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * 문제
+ * 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ * 입력
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * 출력
+ * 첫째 줄에 A-B를 출력한다.
+ * 예제 입력
+ * 3 2
+ * 예제 출력
+ * 1
+ */
+public class Problem1001 {
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        int b = sc.nextInt();
+        sc.nextLine();
+        System.out.println(a-b);
+    }
+}
